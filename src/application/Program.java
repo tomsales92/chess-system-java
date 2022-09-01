a8efd02a58f90cbbe6087b6f392ef486fd66ddb8
Everton Sales <everton.goncalves92@gmail.com>
2022-09-01 09:04:07 -0300

    starting to implement board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,9 +5,8 @@ import boardgame.Position;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		Board board = new Board(8,8);
 	}
 
 }
+ 
